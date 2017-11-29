2fe90d00cba6d585cdf9045c80784995d0a97d64
alexandradutz <alexandratrifaan@gmail.com>
2017-11-29 16:12:09 +0200

    Update make_darwin.sh

--- CI/travis/make_darwin.sh
+++ CI/travis/make_darwin.sh
@@ -10,9 +10,9 @@ cp /usr/local/lib/libqwtpolar.dylib Scopy.app/Contents/Frameworks/
 install_name_tool -change libqwtpolar.dylib @executable_path/../Frameworks/libqwtpolar.dylib Scopy.app/Contents/MacOS/Scopy
 install_name_tool -change /Library/Frameworks/iio.framework @executable_path/../Frameworks/iio.framework Scopy.app/Contents/MacOS/Scopy
 
-/usr/local/opt/qt/bin/macdeployqt Scopy.app -verbose=3 -dmg
-otool -L Scopy.app/Contents/MacOS/Scopy
+#/usr/local/opt/qt/bin/macdeployqt Scopy.app -verbose=3 -dmg
+#otool -L Scopy.app/Contents/MacOS/Scopy
 
-curl --upload-file ./Scopy.dmg https://transfer.sh/scopy-v0.9.dmg
+#curl --upload-file ./Scopy.dmg https://transfer.sh/scopy-v0.9.dmg
 
 
