667a5a2a23dd0773820a66ad612f436984309958
alexandradutz <alexandratrifaan@gmail.com>
2018-01-08 11:16:17 +0200

    Update make_linux.sh

--- CI/travis/make_linux.sh
+++ CI/travis/make_linux.sh
@@ -2,26 +2,26 @@
 
 cd ${TRAVIS_BUILD_DIR}/build
 mkdir -p appdir/usr/bin/decoders
-mkdir -p appdir/usr/lib/python3.5
+mkdir -p appdir/usr/lib/python3.6
 mkdir -p appdir/usr/lib/python2.7
 
 #sudo mv /usr/bin/python3 /usr/bin/python3-old
 sudo apt-get remove --auto-remove python3.4
 #sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-cp /usr/lib/python3.5/* appdir/usr/lib/python3.5/
+cp /usr/lib/python3.6/* appdir/usr/lib/python3.6/
 cp /usr/lib/python2.7/* appdir/usr/lib/python2.7/
 cp ${TRAVIS_BUILD_DIR}/resources/decoders/* appdir/usr/bin/decoders/
 
 echo "import sys,os
 prefix = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
-sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]" > appdir/usr/lib/python3.5/sitecustomize.py
+sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]" > appdir/usr/lib/python3.6/sitecustomize.py
 
 echo "import sys,os
 prefix = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
 sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]" > appdir/usr/lib/python2.7/sitecustomize.py
 
 find . | grep python
-export PYTHONPATH=${TRAVIS_BUILD_DIR}/build/appdir/usr/lib/python3.5
+export PYTHONPATH=${TRAVIS_BUILD_DIR}/build/appdir/usr/lib/python3.6
 
 ls -la /usr/bin | grep python
 python3 --version
